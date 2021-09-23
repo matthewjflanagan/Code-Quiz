554483ccb90dc5dd907f8c0969e4c07a4f31b38f
Matthew Flanagan <mattflanagan40@gmail.com>
2021-09-22 20:11:42 -0400

    Question added via Javascript

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,32 @@
 var startButton = document.getElementById('start-btn')
 var questionContainerEl = document.getElementById('question-container')
+var score = 0;
+
+var correctAnswer = "";
+
+questionContainerEl.innerHTML = "Question: What coding language is best practice to style a webpage?"
+
+// if (startButton === true) {
+//   // Creates question within the container 
+// var question = questionContainerEl.createElement();
+
+// question.textContent = "Question: What coding language is best practice to style a webpage?"
+
+// //Appends question as child of document body
+
+// // document.body.children[1].children[0].appendChild(question);
+
+// }
 
 startButton.addEventListener('click', startGame)
 
 // Function for starting the timer and showing the first question when the Start button is selected
 function startGame() {
-    console.log('Started')
-    startButton.classList.add('hide')
-    questionContainerEl.classList.remove('hide')
-    setNextQuestion()
+    setTime()
+    Question()
 }
 
-// Function for deleting the previous questions and showing the next question when an answer is selected
-function setNextQuestion() {
 
-}
-
-// Function for showing an answer is correct and adding points to total score
-function selectAnswer () {
-
-}
 
 // Selecting the h1 element with the class .time
 var timeEl = document.querySelector(".time");
@@ -42,10 +49,35 @@ function setTime () {
     }, 1000);
 }
 
-// Function to show score and option to enter intials into Highscores 
-function showScore() {
+//  Tests to see if correct answer was selected 
+
+function checkAnswers() {
 
 }
 
-setTime();
 
+var container = document.querySelector(".container");
+
+container.addEventListener("click", function(event) {
+  var element = event.target;
+
+  if (element.matches(".btn")) {
+    score++;
+
+    // Use an if statement to conditionally render the number on the card
+    if (state === "hidden") {
+      // If the card is clicked while the state is "hidden", we set .textContent to the number 
+      element.textContent = element.dataset.number;
+      // Using the dataset property, we change the state to visible because the user can now see the number
+      element.dataset.state = "visible";
+   
+    } else {
+      // 'Hide' the number by setting .textContent to an empty string
+      element.textContent= "";
+      // Use .setAttribute() method
+      element.setAttribute("data-state", "hidden")
+     
+    }  
+  }
+  
+});
