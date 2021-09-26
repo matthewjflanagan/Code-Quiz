f11477bdd76abc832ed667b279607bcb8f86e359
Matthew Flanagan <mattflanagan40@gmail.com>
2021-09-25 22:11:26 -0400

    First Question showing when start button is selected

--- assets/script.js
+++ assets/script.js
@@ -1,83 +1,53 @@
-var startButton = document.getElementById('start-btn')
-var questionContainerEl = document.getElementById('question-container')
-var score = 0;
-
-var correctAnswer = "";
-
-questionContainerEl.innerHTML = "Question: What coding language is best practice to style a webpage?"
-
-// if (startButton === true) {
-//   // Creates question within the container 
-// var question = questionContainerEl.createElement();
-
-// question.textContent = "Question: What coding language is best practice to style a webpage?"
-
-// //Appends question as child of document body
-
-// // document.body.children[1].children[0].appendChild(question);
-
-// }
-
-startButton.addEventListener('click', startGame)
-
-// Function for starting the timer and showing the first question when the Start button is selected
-function startGame() {
-    setTime()
-    Question()
-}
-
-
-
-// Selecting the h1 element with the class .time
-var timeEl = document.querySelector(".time");
-
-// Creating variable of secondsLeft with a starting value of 120 seconds
+var time = document.getElementById("time");
 var secondsLeft = 120;
+var start = document.getElementById("start-btn");
+var mode = "go";
+var question = document.getElementById("question");
+var container = document.getElementById("container");
+var answers = document.getElementById("answers");
+var A = document.getElementById("A");
+var B = document.getElementById("B");
+var C = document.getElementById("C");
+var D = document.getElementById("D");
+
+question.textContent = "Click Start to begin the quiz!";
+
+// Listen for a click event on button
+start.addEventListener('click', function () {
+  // Start time
+  setTime();
+  // If start button is pressed hide it
+  if (mode === "go") {
+    mode = "hide";
+    start.setAttribute("class", "hide");
+  } 
+  // If start button has not been pressed show it
+  else {
+    mode = "go"
+  }
+
+  // Change text to first question
+  question.textContent = "What coding language is best practice for styling a webpage";
+
+  // Change text to answer choices
+  A.textContent = "A: Javascript";
+  B.textContent = "B: CSS";
+  C.textContent = "C: C++";
+  D.textContent = "D: HTML";
+
+});
 
 function setTime () {
     // Setting the interval of countdown in variable timerInterval
     var timerInterval = setInterval(function () {
         secondsLeft--;
-        timeEl.textContent = secondsLeft + " seconds left";
-
+        time.textContent = secondsLeft + " seconds left";
+  
         if(secondsLeft === 0) {
             // Stops execution of action at set interval 0
             clearInterval(timerInterval);
-            // Calls function to show score and option to enter intials into Highscores 
+              // Calls function to show score and option to enter intials into Highscores 
             showScore();
         }
-    }, 1000);
-}
-
-//  Tests to see if correct answer was selected 
-
-function checkAnswers() {
-
-}
-
-
-var container = document.querySelector(".container");
-
-container.addEventListener("click", function(event) {
-  var element = event.target;
-
-  if (element.matches(".btn")) {
-    score++;
-
-    // Use an if statement to conditionally render the number on the card
-    if (state === "hidden") {
-      // If the card is clicked while the state is "hidden", we set .textContent to the number 
-      element.textContent = element.dataset.number;
-      // Using the dataset property, we change the state to visible because the user can now see the number
-      element.dataset.state = "visible";
-   
-    } else {
-      // 'Hide' the number by setting .textContent to an empty string
-      element.textContent= "";
-      // Use .setAttribute() method
-      element.setAttribute("data-state", "hidden")
-     
-    }  
+      }, 1000);
   }
-  
-});
